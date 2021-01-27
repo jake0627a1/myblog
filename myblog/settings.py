dd01ba5358d59c1fe4115dffeac1483ffc846004
jake0627a1 <jake0627a1@gmail.com>
2021-01-27 10:25:30 +0800

    first commit

--- myblog/settings.py
+++ myblog/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'markdown_deux',
     'mainsite',
 ]
 
@@ -122,3 +123,4 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS=[
     os.path.join(BASE_DIR,'static'),
 ]
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
