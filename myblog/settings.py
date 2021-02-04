5843be33a380c904f02351f5073b9a6be153672d
jake0627a1 <jake0627a1@gmail.com>
2021-02-03 19:18:10 -0800

    fix database setting

--- myblog/settings.py
+++ myblog/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 from pathlib import Path
 import os
+import dj_database_url
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -127,3 +128,5 @@ STATIC_ROOT = os.path.join(BASE_DIR,'static')
     os.path.join(BASE_DIR,'static'),
 ]
 STATIC_ROOT = os.path.join(BASE_DIR,'static')
+db_from_emv=dj_database_url.onfig()
+DATABASES['default'].update(db_from_env)
